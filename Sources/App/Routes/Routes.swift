dac6f57db3b8103639058bcad45df264a1a4f8a5
Tuan <tuanphunglk@gmail.com>
2017-10-22 23:45:04 +0700

    Removed unused files

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -4,27 +4,11 @@ let taskController = TaskController()
 
 extension Droplet {
     func setupRoutes() throws {
-        post("task-reminder/receiver", handler: taskController.slackReceiver)
-
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
         get("info") { req in
             return req.description
         }
-
-        get("description") { req in return req.description }
         
-        try resource("posts", PostController.self)
+        post("task-reminder/receiver", handler: taskController.slackReceiver)
         try resource("tasks", TaskController.self)
     }
 }
